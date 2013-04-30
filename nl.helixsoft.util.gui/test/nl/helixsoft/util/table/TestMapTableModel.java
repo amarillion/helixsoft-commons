3b8ca6152f391c1c8a1b5c463e7d9eaa30f27241
Martijn van Iersel <mvaniersel@gmail.com>
2013-04-30 10:14:46 +0100

    Added junit test for nl.helixsoft.gui

---
+++ nl.helixsoft.util.gui/test/nl/helixsoft/util/table/TestMapTableModel.java
@@ -0,0 +1,23 @@
+package nl.helixsoft.util.table;
+
+import junit.framework.TestCase;
+
+public class TestMapTableModel extends TestCase
+{
+	public void test1()
+	{
+		MapTableModel<Integer, Integer> map = new MapTableModel<Integer, Integer>();
+		map.put (4, 16);
+		map.put (5, 25);
+		map.put (6, 36);
+		
+		assertEquals (3, map.getRowCount());
+		
+		assertEquals (5, map.getValueAt(1, 0));
+		assertEquals (25, map.getValueAt(1, 1));
+		
+		assertEquals (6, map.getValueAt(2, 0));
+		assertEquals (36, map.getValueAt(2, 1));
+
+	}
+}
