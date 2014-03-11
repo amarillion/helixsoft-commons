a736887c30aa335fcc6d5db19bf4beda38f39e76
Martijn van Iersel <mvaniersel@gmail.com>
2014-03-11 09:23:16 +0100

    Documentation for backupcull

--- nl.helixsoft.misc/src/nl/helixsoft/misc/BackupCull.java
+++ nl.helixsoft.misc/src/nl/helixsoft/misc/BackupCull.java
@@ -13,10 +13,23 @@ import org.kohsuke.args4j.CmdLineParser;
 import org.kohsuke.args4j.Option;
 
 /**
- * Utility for culling old backup files
+ * Utility for culling old backup files.
+ * <p>
+ * Takes a list of files as input. The files are grouped by age. For each age-group one file is kept, the rest is discarded.
+ * This way you only keep a small collection of historic backups.
+ * <p>
+ * TODO
+ * [ ] Provide command-line options to adjust the bin cut-offs
+ * [ ] Allow working on dated directories instead of files
+ * [ ] Provide command-line option to extract date from filename instead of file creation time.
+ * [ ] Better unit testing.
  */
 public class BackupCull 
 {
+	/**
+	 * Comparator used to decide which file to keep from a Bin.
+	 * We give preference to certain dates, i.e. the first day of the month, on the first month of the year, etc.
+	 */
 	private static class PreferredFileComparator implements Comparator <File>
 	{
 		public int score (File a)
@@ -24,11 +37,11 @@ public class BackupCull
 			int score = 0;
 			LocalDate t = new LocalDate (a.lastModified());
 			int dom = t.getDayOfMonth(); 
-			if ((dom % 7) == 1) score += 5;
-			if (dom == 1) score += 5;
+			if ((dom % 7) == 1) score += 5; // prefer days 1, 8, 15, 22, 29
+			if (dom == 1) score += 5; // prefer day 1 even more
 			int moy = t.getMonthOfYear();
-			if ((moy %3) == 1) score++;
-			if (moy == 1) score++;
+			if ((moy %3) == 1) score++; // prefer months 1, 4, 7, 10
+			if (moy == 1) score++; // prefer month 1 even more
 			return score;
 		}
 		
@@ -39,8 +52,15 @@ public class BackupCull
 		}
 	}
 
+	/**
+	 * A Bin is a collection of Files below a certain age.
+	 * Bins are chained - Files that fall below a cut-off are passed to the next bin.
+	 */
 	private static class Bin
 	{
+		/**
+		 * With andThen... we can create a chain of Bins.
+		 */
 		public Bin andThen (LocalDate d)
 		{
 			next = new Bin();
@@ -48,6 +68,9 @@ public class BackupCull
 			return next;
 		}
 		
+		/**
+		 * Try to add a File to this bin. If it's too old, it's automatically passed to the next bin.
+		 */
 		public void addFile(File f)
 		{
 			LocalDate ftime = new LocalDate (f.lastModified());
@@ -64,13 +87,12 @@ public class BackupCull
 			}
 		}
 
-		public void execute()
-		{
-			execute (true);
-		}
-		
+		/**
+		 * @param dryrun enables dry-run mode, the actions are printed but not actually executed. 
+		 */
 		public void execute(boolean dryrun)
 		{
+			// keep the first File in the priorityQueue (it has the highest score). The others are marked for deletion.
 			boolean first = true;
 			for (File f : files)
 			{
@@ -107,7 +129,7 @@ public class BackupCull
 		PriorityQueue<File> files = new PriorityQueue<File>(16, new PreferredFileComparator());
 	}
 
-
+	/** Command-line options */
 	public static class Options
 	{
 		@Option(name="-n", usage="Dry run. Show actions, but do not execute.")
@@ -122,12 +144,12 @@ public class BackupCull
 	
 	Options opts;
 	
+	/** All the work starts here */
 	public void run(String[] args)
 	{
 		opts = new Options();
 	    CmdLineParser parser = new CmdLineParser(opts);
 	    
-		// depending on task, dump rdf, generate provenance rdf, or execute tasks.
 	    try
 	    {
 	    	parser.parseArgument(args);
@@ -152,7 +174,8 @@ public class BackupCull
 			andThen (today.minus(Period.months(1))).
 			andThen (today.minus(Period.months(3))).
 			andThen (today.minus(Period.years(1))).
-			andThen (today.minus(Period.years(2)));
+			andThen (today.minus(Period.years(2))).
+			andThen (today.minus(Period.years(5)));
 		
 		for (File f : opts.files)
 		{
