65388ba31355858ecc64ee0abb710a2a223f1575
Martijn van Iersel <mvaniersel@gmail.com>
2014-06-09 13:59:10 +0100

    Added functional search to collections

--- nl.helixsoft.util/src/nl/helixsoft/util/CollectionUtils.java
+++ nl.helixsoft.util/src/nl/helixsoft/util/CollectionUtils.java
@@ -2,8 +2,22 @@ package nl.helixsoft.util;
 
 import java.util.Collection;
 
+import nl.helixsoft.recordstream.Predicate;
+
 public class CollectionUtils 
 {
+	/**
+	 * Find the first occurence in the collection that matches the Predicate.
+	 */
+	public static <T> T findFirst(Collection<T> col, Predicate<T> p)
+	{
+		for (T t : col)
+		{
+			if (p.accept(t)) return t;
+		}
+		return null;
+	}
+
 	public static boolean emptyOrNull (Collection<?> c)
 	{
 		return c == null || c.isEmpty();
