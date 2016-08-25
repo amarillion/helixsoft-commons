2e2ba1d5da9e151d4766acf5e5ef28d66b2fc945
Martijn van Iersel <mvaniersel@gmail.com>
2016-08-25 13:47:44 +0200

    Make ResultSetRecordStream close Statement and optionally Connection

--- nl.helixsoft.util/src/nl/helixsoft/recordstream/ResultSetRecordStream.java
+++ nl.helixsoft.util/src/nl/helixsoft/recordstream/ResultSetRecordStream.java
@@ -1,19 +1,52 @@
 package nl.helixsoft.recordstream;
 
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Turn a {@link java.sql.ResultSet} into a {@link RecordStream}.
+ * Underlying resources are closed automatically if you read the stream until the end.
+ * You can also close manually with {@link #close}
+ */
 public class ResultSetRecordStream extends AbstractRecordStream
 {
+	Logger log = LoggerFactory.getLogger("com.generalbioinformatics.rdf.ResultSetRecordStream");
+
 	private final ResultSet rs;
 	private boolean closed = false;
+	
 	private final RecordMetaData rmd;
 	
+	private final Statement wrappedStatement; // may be null
+	private final Connection wrappedConnection; // may be null
+
+	@Deprecated	
+	/** 
+	 * Better use the other constructor, to make sure Statement (and optionally Connection) are closed as well.
+	 */
 	public ResultSetRecordStream(ResultSet wrapped) throws StreamException 
 	{ 
+		this(wrapped, null, null);
+	}
+
+	/** 
+	 * @param wrapped ResultSet to read records from
+	 * @param st Underlying statement, will be closed when this recordstream is closed. 
+	 * @param con Connection. Will be closed when this recordstream is closed. May be null if you want to keep the connection alive. 
+	 */ 
+	public ResultSetRecordStream(ResultSet wrapped, Statement st, Connection con) throws StreamException 
+	{ 
 		this.rs = wrapped;
+		this.wrappedStatement = st;
+		this.wrappedConnection = con;
+		
 		try {
 			int colNum = rs.getMetaData().getColumnCount();
 			List<String> colnames = new ArrayList<String>();
@@ -27,6 +60,7 @@ public class ResultSetRecordStream extends AbstractRecordStream
 		}
 		catch (SQLException ex)
 		{
+			close();
 			throw new StreamException(ex);
 		}
 	}
@@ -40,47 +74,70 @@ public class ResultSetRecordStream extends AbstractRecordStream
 			
 			if (!rs.next()) 
 			{
-				close();
+				close(); // automatically close at end of stream.
 				return null;
 			}
 			
-			Object[] data = new Object[rmd.getNumCols()];
-			for (int col = 1; col <= rmd.getNumCols(); ++col)
-			{
-				data[col-1] = rs.getObject(col);
-			}
-			return new DefaultRecord(rmd, data);
+			Record result = processRow(rs); 
+			return result;
+			
 		} catch (SQLException ex) {
 			throw new StreamException(ex);
 		}
 	}
 
-	@Override public void close() 
+	/** designed to be overridden by subclasses */
+	protected Record processRow(ResultSet rs) throws SQLException 
 	{
+		Object[] data = new Object[rmd.getNumCols()];
+		for (int col = 1; col <= rmd.getNumCols(); ++col)
+		{
+			data[col-1] = rs.getObject(col);
+		}
+		return new DefaultRecord(rmd, data);
+	}
+
+	public final void close() 
+	{
+		if (closed) { log.debug ("Ignoring second close"); return; }
 		closed = true;
 		try {
-			rs.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			rs.close();			
+		} catch (SQLException e) { log.debug ("Exception during close", e); }
+		finally {
+			try
+			{
+				if (wrappedStatement != null)
+				{
+					wrappedStatement.close();
+				}
+			} catch (SQLException e) { log.debug ("Exception during close", e); }
+			finally
+			{
+				if (wrappedConnection != null)
+					try {
+						log.info ("Returning connection to the pool");
+						wrappedConnection.close();
+					} catch (SQLException e) { log.debug ("Exception during close", e); }
+			}
 		}
 	}
 
 	@Override
-	public void finalize()
-	{
-		try {
-			if (!closed) rs.close();
-		} 
-		catch (SQLException e) 
-		{
-			e.printStackTrace();
-		}
-	}
-	
-	@Override
-	public RecordMetaData getMetaData() 
+	public final RecordMetaData getMetaData() 
 	{
 		return rmd;
 	}
+	
+	@Override
+	public void finalize()
+	{
+		// if we close at garbage collection, it's already too late... We missed a close() somewhere.
+		if (!closed) 
+		{
+			log.warn ("ResultSet was not closed properly");
+			close();
+		}
+	}
+
 }
